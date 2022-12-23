f482df1b8370dbc847ffe6b8570ce05efb91788e
Josue David Flores Mena <josueflormen@gmail.com>
2022-12-22 21:59:55 -0600

    Search gif ready

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = (event) => {
+        setInputValue( event.target.value );
+    }
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        const cleanInputValue = inputValue.trim();
+
+        if ( cleanInputValue.length <= 1 ) return;
+
+        onNewCategory( cleanInputValue );
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input 
+            type="text"
+            placeholder="Buscar gifs"
+            value={ inputValue }
+            onChange={ onInputChange }
+            />
+        </form>
+    )
+}
